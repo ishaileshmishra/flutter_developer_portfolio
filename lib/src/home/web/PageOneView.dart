8499eb68a0ee5766b2581d2b512850c212ff5b5e
shaileshmishra <Yes@383840>
2021-03-19 04:59:24 +0530

    added :love:

--- lib/src/home/web/PageOneView.dart
+++ lib/src/home/web/PageOneView.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class PageOneView extends StatefulWidget {
@@ -16,43 +17,87 @@ class _PageOneViewState extends State<PageOneView> {
         alignment: Alignment.centerRight,
         children: [
           // Developer Photograph
-          Positioned(
-              child: Container(
-            child: Image.asset('img/marcelo.png', fit: BoxFit.fill),
-          )),
-
+          Image.asset('img/marcelo.png', fit: BoxFit.fill),
           // Howdy Shailesh
-          Align(
-            alignment: Alignment.center,
+          Positioned(
+            left: 0,
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                RichText(
-                  text: new TextSpan(
-                    children: <TextSpan>[
-                      TextSpan(
-                        text: 'Howdy ',
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 100,
-                            color: Colors.red),
+                Column(
+                  children: [
+                    Container(
+                      padding: EdgeInsets.all(0),
+                      child: RichText(
+                        text: new TextSpan(
+                          children: <TextSpan>[
+                            TextSpan(
+                              text: 'Howdy ',
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 100,
+                                  color: Colors.red),
+                            ),
+                            TextSpan(
+                                text: "I'm \nShailesh",
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 100,
+                                    color: Colors.white)),
+                          ],
+                        ),
                       ),
-                      TextSpan(
-                          text: "I'm \nShailesh",
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 100,
-                              color: Colors.white)),
-                    ],
-                  ),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Row(
+                      children: [
+                        Container(
+                          width: 300,
+                          child: Divider(
+                            color: Colors.white,
+                            thickness: 1,
+                          ),
+                        ),
+                        Icon(
+                          CupertinoIcons.play_circle,
+                          size: 150,
+                        ),
+                        Text(
+                          'Watch Resume',
+                          style: TextStyle(fontSize: 20),
+                        )
+                      ],
+                    )
+                  ],
                 ),
-                Icon(
-                  Icons.slow_motion_video,
-                  size: 150,
-                )
               ],
             ),
           ),
+
+          //Bottom sheet
+          Positioned(
+              bottom: 20,
+              left: 20,
+              right: 20,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text("© Shailesh 2021."),
+                  Row(
+                    children: [
+                      Icon(CupertinoIcons.add),
+                      SizedBox(width: 20),
+                      Icon(CupertinoIcons.alt),
+                      SizedBox(width: 20),
+                      Icon(CupertinoIcons.alarm),
+                      SizedBox(width: 20),
+                      Icon(CupertinoIcons.barcode),
+                    ],
+                  )
+                ],
+              ))
         ],
       ),
     );
